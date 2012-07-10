5a7415873fd8cc536f5e47c4d2af9a76487807bb
Christopher Davis <cdavis9999@gmail.com>
2012-07-09 22:17:41 -0500

    Plugin File & Base Class
    The initial plugin file along with the base class for the whole plugin.
    Basically setting up things for later.

---
+++ advanced-posts-per-page.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Advanced Posts/Page
+Plugin URI: https://github.com/chrisguitarguy/Advanced-Post-Page
+Description: Set how many posts are each page of all your WordPress archives
+Version: 1.0
+Text Domain: cdappp
+Domain Path: /lang
+Author: Christopher Davis
+Author URI: http://christopherdavis.me
+License: GPL2
+
+    Copyright 2012 Christopher Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define('CD_APPP_PATH', plugin_dir_path(__FILE__));
+
+require_once(CD_APPP_PATH . 'inc/base.php');
+if(is_admin())
+{
+    require_once(CD_APPP_PATH . 'inc/reading-settings.php');
+}
---
+++ inc/base.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Base class for Advanced Posts/Page.  Provided functionality and class
+ * constants used throughout the plugin
+ *
+ * @author      Christopher Davis <http://christopherdavis.me>
+ * @since       1.0
+ * @package     Advanced Post/Page
+ * @license     GPLv2
+ * @copyright   Christopher Davis 2012
+ */
+
+class CD_APPP_Base
+{
+    /**
+     * The plugins settings name (used in `register_setting` and such)
+     * 
+     * @since        1.0
+     * @access       public
+     */
+    const SETTING = 'cd_appp_options';
+
+    /**
+     * Container for holding information about the taxonomy posts/page
+     * 
+     * @since        1.0
+     * @access       public
+     */
+    const TAXOPT = 'cd_appp_tax_settings';
+
+    /**
+     * Prefix a post type name for saving in our options
+     * 
+     * @since       1.0
+     * @access      protected
+     * @param       string $pt The post name name
+     * @return      string The post type with the prefix
+     */
+    protected static function prefix_pt($pt)
+    {
+        return sprintf("post_type_%s", esc_attr($pt));
+    }
+
+    /**
+     * Prefix a taxonomy for saving in our options
+     * 
+     * @since       1.0
+     * @access      protected
+     * @param       string $t The taxonomy name to prefix
+     * @return      string The taxonomy with the prefix
+     */
+    protected static function prefix_tax($t)
+    {
+        return sprintf('taxonomy_%s', esc_attr($t));
+    }
+
+    /**
+     * Get and option from `self::SETTING`
+     * 
+     * @since       1.0
+     * @access      protected
+     * @param       string $key The option key
+     * @param       mixed $default The default return value
+     * @return      mixed Whatever happens to be in the option
+     */
+    protected static function opt($key, $default='')
+    {
+        // fetch the option each time, let WP do the caching
+        $opts = get_option(self::SETTING, array());
+        return isset($opts[$key]) ? $opts[$key] : $default;
+    }
+
+    /**
+     * Get an option from `self::TAXOPTS
+     * 
+     * @since       1.0
+     * @access      protected
+     * @param       string $key The option key
+     * @param       mixed $default Whatever the default value is
+     * @return      mixed Whatever happens to be in the setting
+     */
+    protected static function get_taxopt($key, $default)
+    {
+        $opts = get_option(self::TAXOPT, array());
+        return isset($opts[$key]) ? $opts[$key] : $default;
+    }
+
+    /**
+     * Fetch all the post types for which this plugin will have settings
+     *
+     * @since       1.0
+     * @access      protected
+     * @return      array Prefixed post type as the key and label as the value
+     */
+    protected static function post_types()
+    {
+        $types = get_post_types(array(
+            'public'      => true,
+            '_builtin'    => false, // no default post types
+            'has_archive' => true
+        ), 'objects');
+
+        if(!$types)
+            return false;
+
+        $rv = array();
+        foreach($types as $n => $t)
+        {
+            $rv[self::prefix_pt($n)] = $t->label;
+        }
+        return apply_filters('cd_appp_post_types', $rv);
+    }
+
+    /**
+     * Fetch all the taxonomies for which this plugin will have settings
+     *
+     * @since       1.0
+     * @access      protected
+     * @return      array Prefixed taxonomy as the key, tax name as the value
+     */
+    protected static function taxonomies()
+    {
+        $taxes = get_taxonomies(array(
+            'public'  => true
+        ), 'objects');
+        if(!$taxes)
+            return false;
+
+        $rv = array();
+        foreach($taxes as $n => $t)
+        {
+            $rv[self::prefix_tax($n)] = $t->label;
+        }
+        return apply_filters('cd_appp_taxonomies', $rv);
+    }
+
+    /**
+     * Get the fields for the default section settings
+     *
+     * @since       1.0
+     * @access      protected
+     * @return      array Settings name as the key, label as the value
+     */
+    protected static function archives()
+    {
+        $rv = array(
+            'archives'    => __('Default Archive Posts per Page', 'cdappp'),
+            'date_year'   => __('Yearly Archives', 'cdappp'),
+            'date_month'  => __('Monthly Archives', 'cdappp'),
+            'date_day'    => __('Daily Archives', 'cdappp'),
+            'author'      => __('Author Archives', 'cdappp'),
+            'search'      => __('Search Results', 'cdappp')
+        );
+        return apply_filters('cd_appp_archives', $rv);
+    }
+
+    /**
+     * Gett a settins field name based on the key
+     *
+     * @since       1.0
+     * @access      protected
+     * @return      string
+     */
+    protected static function prefix_opt($key)
+    {
+        return sprintf('%s[%s]', self::SETTING, esc_attr($key));
+    }
+}
